45bba90231b9b5637ca8ac9965e49b43aebb943f
timittt <timpantoffelmann@gmail.com>
2026-02-18 20:21:46 +0100

    make user enter desired refueling amount

--- courses/coding-in-C/Lab_6/car-refuel.c
+++ courses/coding-in-C/Lab_6/car-refuel.c
@@ -9,18 +9,24 @@ struct Car {
 
 void refuel(struct Car *p, int add_fuel)
 {
-    if(p->fuel_level + add_fuel > p->max_fuel_level)
+    if((p->fuel_level + add_fuel) > p->max_fuel_level)
     {
         printf("Can't add this much fuel!\n");
     }
     else
     {
         p->fuel_level += add_fuel;
+        printf("Refueled %d l. Your %s is now %.1f%% filled (%.1f l).\n",
+        add_fuel, p->name, 100*(p->fuel_level/p->max_fuel_level), p->fuel_level);
     }
 }
 
 int main()
 {
-    struct Car Touran = {0.5, 55, "VW Touran"};
+    struct Car myCar = {0.5, 55, "VW Touran"};
+    printf("Enter amount to refuel: ");
+    int add = 0;
+    scanf("%d", &add);
+    refuel(&myCar, add);
     return 0;
 }
