a613804f8b2e755b844b373d1634527ce8bc1fb2
timittt <timpantoffelmann@gmail.com>
2026-02-18 09:39:30 +0100

    improve pointer usage

--- courses/coding-in-C/Lab_5/args.c
+++ courses/coding-in-C/Lab_5/args.c
@@ -11,8 +11,8 @@ int main(int argc, char **argv)
 
     for(int i = 0; i < argc; i++)
     {
-        unsigned short len = strlen((argv + i));
-        printf("Argument %d at address %p contains \"%s\" (length %d)\n", i, (argv + i), (argv + i), len);
+        unsigned short len = strlen(*(argv + i));
+        printf("Argument %d at address %p contains \"%s\" (length %d)\n", i, *(argv + i), *(argv + i), len);
     }
     return 0;
 }
