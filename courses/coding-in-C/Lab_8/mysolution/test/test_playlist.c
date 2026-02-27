8b66f56ec3151c99c912b27d7d1ed52f26d4df06
timittt <timpantoffelmann@gmail.com>
2026-02-27 17:48:58 +0100

    add test for add_song_index

--- courses/coding-in-C/Lab_8/mysolution/test/test_playlist.c
+++ courses/coding-in-C/Lab_8/mysolution/test/test_playlist.c
@@ -30,6 +30,41 @@ void test_add_song(void)
     delete_playlist(&p);
 }
 
+void test_add_song_index(void)
+{
+    Playlist p;
+    init_playlist(&p);
+    add_song_index("Tirol", "DJ Ötzi", 3, &p); //should not be added (invalid index)
+    
+    assert(p.size == 0);
+
+    add_song("First Song", "First Artist", &p);
+    add_song("Second Song", "Second Artist", &p);
+    add_song("Third Song", "Third Artist", &p);
+    add_song("Fourth Song", "Fourth Artist", &p);
+    add_song_index("Tirol", "DJ Ötzi", 3, &p); // should be added between second and third song
+    add_song_index("Tirol", "DJ Ötzi", 0, &p); //should not be added (invalid index)
+    assert(p.size == 5);
+    
+    Song *temp = p.head;
+    for(int i = 0; i < 3; i++) // go to index 3
+    {
+        temp = temp->next;
+    }
+    assert(temp != NULL);
+    assert(strcmp(temp->title, "Tirol") == 0);
+    
+    temp = temp->next; // go to index 4
+    assert(temp != NULL);
+    assert(strcmp(temp->artist, "Third Artist") == 0);
+
+    temp = temp->next; // go to index 5
+    assert(temp != NULL);
+    assert(strcmp(temp->title, "Fourth Song") == 0);
+
+    delete_playlist(&p);
+}
+
 void test_delete_firstSong(void)
 {
     Playlist p;
@@ -86,6 +121,7 @@ void test_max_songs_limit(void)
 
     delete_playlist(&p);
 }
+
 /* === Test-Runner === */
 
 int main(void)
@@ -96,6 +132,7 @@ int main(void)
     test_delete_firstSong_empty(); // what happens if we delete first song from empty playlist
     test_delete_playlist();
     test_max_songs_limit(); // verify if the limit will be not be surpassed
+    test_add_song_index();
 
     printf("Alle Playlist-Tests erfolgreich bestanden.\n");
     return 0;
