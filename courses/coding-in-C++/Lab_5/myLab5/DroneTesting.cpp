53217603dae1e82b293604dfec5b3790112ad31b
timittt <timpantoffelmann@gmail.com>
2026-05-25 00:26:44 +0200

    do tasks 4 through 8

--- courses/coding-in-C++/Lab_5/myLab5/DroneTesting.cpp
+++ courses/coding-in-C++/Lab_5/myLab5/DroneTesting.cpp
@@ -6,9 +6,24 @@ int main()
 	std::string name2 = "Thomas";
 	swapValues(name, name2);
 
-	char array[5] = { 'T', 'H', 'g', 'Y', 'z' };
-	double array2[3] = { 1.2, 3.33333, 4.6 };
-	printArray(array, 5);
-	printArray(array2, 3);
-	std::cout << getMinimum(array2, 3) << std::endl;
+	char array[] = { 'T', 'H', 'g', 'Y', 'z' , 'P'};
+	double array2[] = { 1.2, 3.33333, 0.6 , 0.3, 2.2};
+	printArray(array);
+	printArray(array2);
+	std::cout << getMinimum(array2) << std::endl;
+	printPair("X", "Retard", "Y", 22.2);
+
+	std::vector<int> flight_recorder = { 42, 17, 42, 5, 99, 17, 63, 12 };
+	std::cout << "Recorded Data: ";
+	printVector(flight_recorder);
+	std::sort(flight_recorder.begin(), flight_recorder.end());
+	std::cout << "Sorted   Data: ";
+	printVector(flight_recorder);
+	searchVector(flight_recorder, 62);
+	searchVector(flight_recorder, 63);
+
+	std::vector<int> test_container = { 7, -1, 13, -1, 21, 21, 8, -1, 8 };
+	printVector(test_container);
+	cleanupVector(test_container);
+	printVector(test_container);
 }
--- courses/coding-in-C++/Lab_5/myLab5/DroneTesting.hpp
+++ courses/coding-in-C++/Lab_5/myLab5/DroneTesting.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iterator>
+#include <algorithm>
 
 template<typename T>
 
@@ -13,9 +15,9 @@ void swapValues(T &valA, T &valB)
 	std::cout << "After: Value A = " << valA << ", Value B = " << valB << std::endl;
 }
 
-template <typename T>
+template <typename T, std::size_t size>
 
-void printArray(const T array, const int size)
+void printArray(const T (&array)[size])
 {
 	std::cout << "[";
 	for (int i = 0; i < size; i++)
@@ -26,16 +28,19 @@ void printArray(const T array, const int size)
 	std::cout << std::endl;
 }
 
-template <typename T>
+template <typename T, std::size_t N>
 
-T getMinimum(const T* array, const int size)
+T getMinimum(const T (&array)[N])
 {
-	T minVal = array[0];
-	for (int i = 1; i < size; i++)
+	auto it_start = std::begin(array);
+	auto it_end = std::end(array);
+
+	T minVal = *it_start;
+	for (auto it = ++it_start; it != it_end; ++it)
 	{
-		if (array[i] < minVal)
+		if (*it < minVal)
 		{
-			minVal = array[i];
+			minVal = *it;
 		}
 	}
 	return minVal;
@@ -46,4 +51,45 @@ template <typename T, typename U>
 void printPair(std::string labA, T valA, std::string labB, U valB)
 {
 	std::cout << labA << ": " << valA << " | " << labB << ": " << valB << std::endl;
+}
+
+template <typename T>
+void printVector(const std::vector<T>& v)
+{
+	std::cout << "[";
+	for (T value : v)
+	{
+		std::cout << value << ", ";
+	}
+	std::cout << "end]" << std::endl;
+}
+
+template <typename T>
+bool searchVector(const std::vector<T>& v, const T prompt)
+{
+	if (std::find(v.begin(), v.end(), prompt) != v.end())
+	{
+		std::cout << "Vector contains value " << prompt << std::endl;
+		return true;
+	}
+	std::cout << "Vector does not contain value " << prompt << std::endl;
+	return false;
+}
+
+template <typename T>
+int cleanupVector(std::vector<T>& v)
+{
+	int error_count = 0;
+
+	for (auto it = v.begin() + 1; it != v.end(); it++)
+	{
+		if (*it == -1)
+		{
+			*it = 0;
+			error_count++;
+		}
+	}
+	std::reverse(v.begin(), v.end());
+	std::cout << "Corrected " << error_count << " false values and reversed data set.\n";
+	return error_count;
 }
